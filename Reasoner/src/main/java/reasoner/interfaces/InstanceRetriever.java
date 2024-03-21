7ed4af8e78c9c3063619da4e6a8727ebfc2ddc92
JustinMuecke <justin.muecke@gmail.com>
2024-03-21 12:58:30 +0100

    [CHORE] Delete Duplicate Files

--- Reasoner/src/main/java/reasoner/interfaces/InstanceRetriever.java
+++
@@ -1,9 +0,0 @@
-package reasoner.interfaces;
-
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.reasoner.NodeSet;
-
-public interface InstanceRetriever {
-    NodeSet<OWLNamedIndividual> getInstances(OWLClassExpression owlClassExpression, boolean b);
-}
